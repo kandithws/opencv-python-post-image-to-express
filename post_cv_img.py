b6a55a92a01e062b852fa6b13e53fee4f807fe30
Kandith Wongsuwan <ws.kandith@gmail.com>
2018-04-04 15:21:08 +0700

    fix minor details in pythonfile

--- post_cv_img.py
+++ post_cv_img.py
@@ -5,13 +5,13 @@ import cv2 # opencv3
 from datetime import datetime
 import requests
 
-class CVMatFileStub:
+class CVMatFile:
     def __init__(self, mat_img, encoding='.jpg'):
         self._img = mat_img
         self._encoding = encoding
 
     def read(self):
-        _, buffer = cv2.imencode(self._encoding, img)
+        _, buffer = cv2.imencode(self._encoding, self._img)
         return buffer
     
 
@@ -26,16 +26,16 @@ def publish_json_with_image(data, image_path, url):
 def publish_json_with_cv_mat(data, mat_img, url):
     files = {
         'json': (None, json.dumps(data), 'application/json'),
-        'image': ('filename',CVMatFileStub(mat_img), 'image/jpeg')
+        'image': ('filename', CVMatFile(mat_img), 'image/jpeg')
     }
     r = requests.post(url, files=files)
     print(r.content)
 
 
 if __name__ == '__main__':
-    img = cv2.imread('example.jpg',cv2.IMREAD_COLOR)
-    data = {'license_plate': '6PYV308', 'camera_id': '12345', 'local_time': datetime.now().strftime('%Y-%m-%d %H:%M:%S') }
+    img_mat = cv2.imread('example.jpg',cv2.IMREAD_COLOR)
+    data = {'license_plate': 'W400M', 'camera_id': '12345', 'local_time': datetime.now().strftime('%Y-%m-%d %H:%M:%S') }
     # publish_json_with_image(data, 'example.jpg' ,'http://localhost:3000/api/publish_license')
-    publish_json_with_cv_mat(data, img, 'http://localhost:3000/api/publish_license')
+    publish_json_with_cv_mat(data, img_mat, 'http://localhost:3000/api/publish_license')
     print "Procedure Done!"
 
